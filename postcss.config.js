d5083b7a3c6fa328d6deb7ba73aebb6f21899b5d
dmilanop <dmilanop28@gmail.com>
2023-06-06 13:52:07 -0400

    Modificacion de postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,7 @@
 module.exports = {
     plugins: {
         tailwindcss: {
-            content: [
-                "./index.html",
-                "./src/**/*.{vue,js,ts,jsx,tsx}",
-            ],
-            theme: {
-                extend: {},
-            },
-            plugins: [
-                require('@tailwindcss/forms'),
-            ],
+            tailwindcss: require("./tailwind.config.js"),
         },
         autoprefixer : {},
     },
