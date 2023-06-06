adb457cabd8d7ecf6b5836a93678dd6bdfc245d1
dmilanop <dmilanop28@gmail.com>
2023-06-06 13:47:52 -0400

    Modificacion de postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
 module.exports = {
     plugins: {
-        "tailwindcss" :{},
-        "autoprefixer" : {},
+        tailwindcss: {
+            content: [
+                "./index.html",
+                "./src/**/*.{vue,js,ts,jsx,tsx}",
+            ],
+            theme: {
+                extend: {},
+            },
+            plugins: [
+                require('@tailwindcss/forms'),
+            ],
+        },
+        autoprefixer : {},
     },
 };
